a85bcf05a4d2d42c4b2c5e40748b40bd59f0bd41
nurzhank <asapnrz@gmail.com>
2024-06-16 19:22:32 +0500

    upd django drf

---
+++ product/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+from models import Product, Category
+
+
+class ProductSerializer(serializers.Serializer):
+    class Meta:
+        model = Product
+        fields = ('name', 'description', 'price', 'stock', 'category')
+
+
+
+
+class CategorySerializer(serializers.Serializer):
+
+    class Meta:
+        model = Category
+        fields = ('name', 'description')
--- product/views.py
+++ product/views.py
@@ -1,5 +1,35 @@
+from django.forms import model_to_dict
 from django.shortcuts import render
+from rest_framework import views, request, generics
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+
+from models import Product
+from product.serializers import ProductSerializer
+
 
 # Create your views here.
 
 
+
+class ProductListAPIview(generics.ListCreateAPIView):
+
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+class ProductDetailAPIview(generics.RetrieveAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+# class ProductListAPIview(views.APIView):
+#
+#     def get(self, response, *args, **kwargs):
+#         products = Product.objects.all()
+#         serializer = ProductSerializer(products, many=True)
+#
+#         return Response(serializer.data)
+#
+#
